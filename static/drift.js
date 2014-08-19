b09745880df3ba776e5bc2a5f0753b820584ef26
mobeets <mobeets@gmail.com>
2014-08-19 16:17:31 -0500

    working kinda

--- static/drift.js
+++ static/drift.js
@@ -1,17 +1,18 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 500 - margin.left - margin.right,
     height = 300 - margin.top - margin.bottom;
 
-var x = d3.scale.linear()
+var x = d3.scale.log()
     .range([0, width]);
 
-var y = d3.scale.linear()
+var y = d3.scale.log()
     .range([height, 0]);
 
 var color = d3.scale.category10();
 
 var xAxis = d3.svg.axis()
     .scale(x)
+    .ticks(20, ",.1s")
     .orient("bottom");
 
 var yAxis = d3.svg.axis()
@@ -23,6 +24,11 @@ var svg = d3.select("#chart").append("svg")
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var allData;
+var curS = 1;
+var curK = 1;
+var curA = 1;
 
 // var legend = svg.selectAll(".legend")
 //   .data(color.domain())
@@ -43,7 +49,7 @@ var svg = d3.select("#chart").append("svg")
 //   .style("text-anchor", "end")
 //   .text(function(d) { return d; });
 
-function showData(error, data) {
+function initData(data) {
   data.forEach(function(d) {
     // T,TND,N,S,K,A,i,thresh
     d.T = +d.T;
@@ -52,8 +58,11 @@ function showData(error, data) {
     d.S = +d.S;
     d.K = +d.K;
     d.A = +d.A;
-    d.i = +d.i;
-    d.thresh = +d.thresh;
+    d.i = (+d.i + 1)*1000;
+    d.thresh = (+d.thresh)*100;
+    if (isNaN(d.thresh)) {
+      d.thresh = "1";
+    }
   });
 
   x.domain(d3.extent(data, function(d) { return d.i; })).nice();
@@ -79,30 +88,61 @@ function showData(error, data) {
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("threshold")
-
-  svg.selectAll(".dot")
-      .data(data)
-    .enter().append("circle")
-      .attr("class", "dot")
-      .attr("r", 3.5)
-      .attr("cx", function(d) { return 1; }) // x(d.sepalWidth);
-      .attr("cy", function(d) { return 1; }) // y(d.sepalLength);
-      .style("fill", function(d) { return color(1); }); // color(d.species);
+      .text("threshold");
 
 }
 
-d3.tsv("static/data.csv", showData);
+function showData (data) {
+  var circle = svg.selectAll(".dot").data(data);
+  circle.enter().append("circle")
+      .attr("class", "dot")
+      .attr("r", 3.5)
+      .attr("cx", function(d) { return x(d.i); }) // x(d.sepalWidth);
+      .attr("cy", function(d) { return y(d.thresh); }) // y(d.sepalLength);
+      .style("fill", function(d) { return color(1); }); // color(d.species);  
+  circle.exit().remove();
+  console.log([curS, curK, curA]);
+  console.log(data);
+}
 
-function slider(id) {
-    d3.select(id).call(d3.slider().min(1).max(2).step(1).on("slide", function(evt, value) {
-      d3.select(id + 'textmin').text(value[ 0 ]);
-      d3.select(id + 'textmax').text(value[ 1 ]);
-    }));
+function filterAndShowData() {
+  curData = allData.filter(function (d) { return (d.S == curS) && (d.K == curK) && (d.A == curA); });
+  showData(curData);
+}
+
+function loadData(error, data) {
+  allData = data;
+  initData(allData);
+  filterAndShowData();
+}
+
+d3.csv("static/data.csv", loadData);
+
+function updateS(evt, value) {
+  curS = +value;
+  filterAndShowData();
+}
+function updateK(evt, value) {
+  curK = +value;
+  filterAndShowData();
+}
+function updateA(evt, value) {
+  curA = +value;
+  filterAndShowData();
+}
+
+function initSliderS(id) {
+  d3.select(id).call(d3.slider().min(1).max(4).step(1).on("slide", updateS));
+}
+function initSliderK(id) {
+  d3.select(id).call(d3.slider().min(1).max(4).step(1).on("slide", updateK));
+}
+function initSliderA(id) {
+  d3.select(id).call(d3.slider().min(1).max(4).step(1).on("slide", updateA));
 }
 
 $(function() {
-    slider('#slider1');
-    slider('#slider2');
-    slider('#slider3');
+    initSliderS('#slider1');
+    initSliderK('#slider2');
+    initSliderA('#slider3');
 });
